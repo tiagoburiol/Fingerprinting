18db4ebc5b4b9030a36eb2a7836afe958d0c91b9
tiagoburiol <tiagoburiol@gmail.com>
2023-04-13 10:58:42 -0300

    atualizações

--- fingerprint.py
+++ fingerprint.py
@@ -18,15 +18,22 @@ A Python implementation of Clarke and Minella (2016) method of measures the
 increase in uncertainty when sampling sediment fingerprint
 """
 class Fingerprint:
-
+    
+    '''Inicialize'''
     def __init__(self, filename):
         self.df = pd.read_excel(filename, sheet_name=None)
+        self.D = np.array(self.df['a_fonte(D)'].values)
+        self.E = np.array(self.df['a_fonte(E)'].values)
+        self.L = np.array(self.df['a_fonte(L)'].values)
+        self.Y = np.array(self.df['Sedimentos(Y)'].values)
+        self.Si = np.linalg.inv(np.cov(self.Y.T))
         return None
+        
     
     def infos(self):
         for key in self.df:
             print ("Sheet name:", key)
-            print ("Columns:", self.df[key].columns.ravel())
+            print ("Columns:", self.df[key].columns.tolist())
             print ("Number of samples:", self.df[key].shape[0])
             print("--")
     
@@ -52,130 +59,372 @@ class Fingerprint:
         A[:g,:g] = np.dot(a,a.T)
         A[g, g]=0
         return A     
+        
+        
+    '''
+    ----------------------------------------------------------------
+    SOLVERS OPTIONS: Solvers for the sobredetermined 
+    ----------------------------------------------------------------
+    '''
+    def solve_clarke2016(self,y,d,e,l, normalize=True):
+        if normalize == True:
+             d=d/y; e=e/y; l=l/y; y=y/y
+        a = np.array([d/y,e/y,l/y])
+        A = self._buildSourceContribMatrix(a)
+        Z = np.append(np.dot(a, y/y), 1)
+        P = np.dot(np.linalg.inv(A),Z)
+        #P = solve(A, Z)
+        return (P) 
+        
+    def solve_ols_2x2(self,y,d,e,l, normalize = True):
+        if normalize == True:
+            d=d/y; e=e/y; l=l/y; y=y/y
+        A =  np.array([(d-l).T , (e-l).T]).T
+        y=y-l
+        #P = np.dot((np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)),y-l)
+        P = np.linalg.lstsq(A, y, rcond=None)[0]
+        P = np.append(P,1-P[0]-P[1])
+        return P  
+
+    def solve_ols_4x4(self,y,d,e,l, normalize=True):
+        if normalize == True:
+             d=d/y; e=e/y; l=l/y; y=y/y
+        X = np.array([d/y,e/y,l/y])
+        A = np.vstack([X.T, np.ones(3)])
     
-    def plot2D(self, P, marker=',', mean = False, convex_hull = False):
+        AtA = np.dot(A.T,A)
+        AtA = np.vstack([AtA, np.ones(3)])
+        AtA = np.vstack([AtA.T, np.ones(4)]).T
+        AtA[-1,-1] = 0
+    
+        y = np.append(y,[1])
+        y = y[:, np.newaxis]
+        
+        Aty = np.dot(A.T,y)
+        Aty = np.append(Aty,[1])
+
+        P = np.dot(np.linalg.inv(AtA),Aty)
+        return (P)
+        
+    def solve_gls_4x4(self,y,d,e,l,S_inv):
+        #Xy = np.array([d/y,e/y,l/y]).T
+        X = np.array([d,e,l]).T
+        #S_inv = np.linalg.inv(np.cov(X.T))
+  
+        B = S_inv.dot(X)
+        C = S_inv.dot(y)
+        AtA = np.dot(X.T,B)
+        yty = np.dot(X.T,C)
+        AtA = np.vstack([AtA, np.ones(3)])
+        AtA = np.vstack([AtA.T, np.ones(4)]).T
+        AtA[-1,-1] = 0
+        Aty = np.append(yty,[1])
+        Ps = np.dot(np.linalg.inv(AtA),Aty)
+        return (Ps, X, y, AtA, Aty)
+        
+    def solve_minimize2(self,y,d,e,l):
+        from scipy.optimize import minimize, Bounds, LinearConstraint
+        A =  np.array([d.T ,e.T, l.T]).T
+        P0 = np.array([0.3, 0.3, 0.3])
+        #X0 = -np.log((1./P0)-1.)
+        def f(P):   
+            return sum(((y-np.dot(A,P))/y)**2)
+        lc = LinearConstraint([1,1,1], [1], [1])
+        bnds = Bounds([0,0,0],[1,1,1])
+        S = minimize(f, P0, 
+                     method='trust-constr', 
+                     #jac=rosen_der, 
+                     #hess='cs',
+                     constraints=lc,
+                     options={'verbose': 0},
+                     bounds=bnds)
+        #P = 1.0/(1.0+np.exp(-S.x))
+        P = S.x
+        return(P)
+        
+    '''
+    ----------------------------------------------------------------------
+    DISTANCE OPTIONS: Use scipy.spatial.distance implemantation for 
+    compute distance between two numeric vectors. 
+    - braycurtis: Compute the Bray-Curtis distance between two 1-D arrays.
+    - euclidian: Computes the Euclidean distance between two 1-D arrays.
+    - mahalanobis: Compute the Mahalanobis distance between two 1-D arrays. 
+    -----------------------------------------------------------------------
+    '''
+    def mahalanobis_dist(self, P_arr, Pm):
+        from scipy.spatial import distance
+        S = np.cov(P_arr)
+        dist = []
+        for P in P_arr.T:
+            d = distance.mahalanobis(P, Pm, np.linalg.inv(S))       
+            dist.append(d)
+        return np.array(dist)
+        
+    def braycurtis_dist(self,P_arr):
+        from scipy.spatial import distance
+        Pm = np.mean(P_arr, axis=1)
+        #S = np.cov(P_arr)
+        dist = []
+        for P in P_arr.T:
+            #S = np.cov(P,Pm)
+            d = distance.braycurtis(P, Pm)
+            dist.append(d)
+        return np.array(dist)
+        
+    def euclidean_dist(self,P_arr):
+        from scipy.spatial import distance
+        Pm = np.mean(P_arr, axis=1)
+        
+        dist = []
+        for P in P_arr.T:
+            d = distance.euclidean(P, Pm)
+            dist.append(d)
+        return np.array(dist)
+        
+    '''
+    Calculate de confidence region
+    '''    
+    def confidence_region(self, P, p = 95, spacedist= 'mahalanobis'):
+        if P.shape[0]>2:
+           P=P[0:2,:]
+        Pm = np.mean(P, axis=1)
+        if spacedist=='mahalanobis':
+            dist = self.mahalanobis_dist(P, Pm)
+        if spacedist=='euclidean':
+            dist = self.euclidean_dist(P)
+        
+        #dist = self.mahalanobis_dist(P, Pm)
+        sorted_idx = np.argsort(dist)
+        Psorted = P.T[sorted_idx].T
+        end_idx = int((p/100)*len(Psorted.T))
+        return (Psorted[:,:end_idx])
+        
+        
+        
+        
+    
+    def draw_hull(self, P, title = "Convex Hull", savefig = True,
+                        xlabel = "P1", ylabel="P2"):
+        from scipy.spatial import ConvexHull, convex_hull_plot_2d
+        points = P.T   
+        hull = ConvexHull(points)
+
+        fig = plt.figure(figsize=(6, 4))
+    
+        for simplex in hull.simplices:
+            plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+
+        #plt.plot(Pm[0],Pm[1], "ro")
+        plt.plot(P[0],P[1], "k," )
+    
+        plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'k-')
+        plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'k')
+        plt.title(title)
+        plt.xlabel('P1')
+        plt.ylabel('P2')
+        plt.xlim([-0.1, 0.9])
+        plt.ylim([-0.1, 0.9])
+        if savefig == True:
+            plt.savefig(title+'.png')
+        plt.show()
+        return hull
+    
+    
+    def plot2D(self, P, marker=',', mean = False, convex_hull = False, 
+                     title = "Scatter plot: P1 and P2", savefig= True,
+                     xlabel = "P1", ylabel= "P2"):
+        
+        #fig = plt.figure(figsize=(6, 6))
         if mean==True: 
             Pm = np.mean(P, axis=1)
-            plt.plot(Pm[0],Pm[1], "r.")
+            plt.plot(Pm[0],Pm[1], "ko")
             
         if convex_hull ==True:
             from scipy.spatial import ConvexHull
             points = P.T   
             hull = ConvexHull(points)
             for simplex in hull.simplices:
-                #plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
-                plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r-', lw=1)
-                plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
+                plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+                #plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r-', lw=1)
+                #plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
+                #plt.xlim([-0.1, 0.9])
+                #plt.ylim([-0.1, 0.9])
+                #plt.axis('equal')
 
             print ("Área:", hull.volume) # para convex hull 2d é a área
-            
-        plt.plot(P[0],P[1], ",")
-        plt.grid()
+        ax = plt.gca() #you first need to get the axis handle
+        ax.set_aspect(1.0) #sets the height to width ratio to 1.5. 
+        plt.xlim(-0.1, 1)
+        plt.ylim(-0.1, 1)
+        plt.plot(P[0],P[1], "k,")
+        plt.grid()                
+        plt.title(title+' ('+str(P.shape[1])+')')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        #plt.axis('equal')
+
+        
+        if savefig == True:
+            plt.savefig(title+'.png')
         plt.show()
 
-            
-    def run(self, nY, nD, nE, nL):
-        #from numpy.linalg import inv
-        D = np.array(self.df['a_fonte(D)'].values)
-        E = np.array(self.df['a_fonte(E)'].values)
-        L = np.array(self.df['a_fonte(L)'].values)
-        Y = np.array(self.df['Sedimentos(Y)'].values)
-        
-        P1 = np.array([])
-        P2 = np.array([])
-        P3 = np.array([])
-        nsamples = nY*nD*nE*nL
-        #print ("Número de amostras:", nsamples)
-        #print ("Rodando... aguarde")
-        Ys = Y[np.random.choice(len(Y), nY, replace=False)] # toma uma amostra aleatória de Y
-        Ds = D[np.random.choice(len(D), nD, replace=False)] # toma uma amostra aleatória de D
-        Es = E[np.random.choice(len(E), nE, replace=False)] # toma uma amostra aleatória de E 
-        Ls = L[np.random.choice(len(L), nL, replace=False)] # toma uma amostra aleatória de L
-        
-        for i in range (nsamples):
-            randY = Ys[randrange(len(Ys))]    # toma uma amostra aleatória de randY
-            randD = Ds[randrange(len(Ds))]    # toma uma amostra aleatória de randD 
-            randE = Es[randrange(len(Es))]    # toma uma amostra aleatória de randE 
-            randL = Ls[randrange(len(Ls))]    # toma uma amostra aleatória de randL
-    
-            # Cria uma matriz com as contribuições (ai) normalizadas de D, E e L nas linhas 
-            # Cada vetor linha está sendo dividido por randY para normalizar
-            a = np.array([randD/randY, randE/randY, randL/randY])
-    
-            # Chama a função para montar a matriz A 
-            A = self._buildSourceContribMatrix(a)
-    
-            # Cria o vetor Z
-            Z = np.append(np.dot(a, randY/randY), 1)
-    
-    
-            # Resolve o sistema para AP = Z para a solução P
-            #P = np.dot(inv(A),Z)
-            P = solve(A, Z)
-            
-        
-            # Inclui apenas valores em que P1,P2>0 e P1+P2<1
-            if P[0]>0 and P[1]>0 and 1-P[1]-P[0]>0:
-                P1 = np.append(P1, P[0])
-                P2 = np.append(P2, P[1])
-                P3 = np.append(P3, 1-P[1]-P[0])
 
+    '''
+    Randomly takes a subset of samples from the defined number 
+    of elements of each sample set
+    '''
+    def randon_choice(self, nY, nD, nE, nL):
+        Ys = self.Y[np.random.choice(len(self.Y), nY, replace=False)] 
+        Ds = self.D[np.random.choice(len(self.D), nD, replace=False)] 
+        Es = self.E[np.random.choice(len(self.E), nE, replace=False)] 
+        Ls = self.L[np.random.choice(len(self.L), nL, replace=False)] 
+        return Ys,Ds,Es,Ls
+
+    '''
+    Run
+    '''
+    def run(self, Ys,Ds,Es,Ls, solve=0):
+        P1 = []; P2 = []; P3 = []
+        nY = len(Ys); nD = len(Ds); nE = len(Es); nL = len(Ls)
+        S_inv = self.Si
+        for i in range (nY):
+            y = Ys[i]
+            for j in range (nD): 
+                d = Ds[j]
+                for k in range(nE):
+                    e = Es[k]
+                    for w in range(nL):
+                        l = Ls[w]
+                        if solve==0:
+                            P = self.solve_clarke2016(y,d,e,l, normalize=True)
+                        elif solve==1:
+                            #P = self.solve_minimize2(y,d,e,l)
+                            P = self.solve_ols_4x4(y,d,e,l, normalize=True)
+                        elif solve==2:
+                            P = self.solve_gls_4x4(y,d,e,l,S_inv)[0]
+                        elif solve==3:
+                            P = self.solve_minimize2(y,d,e,l)
+                        # Inclui apenas valores em que P1,P2>0 e P1+P2<1
+                        if P[0]>0 and P[1]>0 and P[2]>0:
+                            if P[0]<1 and P[1]<1 and P[2]<1:
+                                P1.append(P[0])
+                                P2.append(P[1])
+                                P3.append(P[2])
+                                        #print (P[0], P[1], P[2])
         #print("Quantidade de soluções viáveis:", len(P1))
-        return np.array([P1,P2])
+        return np.array([P1, P2, P3])
             
-    def confidence_region(self, P, p = 95):
-        Pm = np.mean(P, axis=1)
-        #print ("P médio: Pm=",Pm)
+ 
+           
         
-        #desvios
-        vect = np.array([P[0]-Pm[0], P[1]-Pm[1]]) 
         
-        S = np.cov(P)
-        #print(vect.shape)
-        dist = []
-        for v in vect.T:
-            d = np.dot(np.dot(v.T, np.linalg.inv(S)), v)
-            dist.append(d)
-        
-        # distâncias
-        dist = np.array(dist)
-        #dist = np.linalg.norm(vect.T, axis=1)
-        #print ("Distâncias:", dist)
-        
-        sorted_idx = np.argsort(dist)
-        Psorted = P.T[sorted_idx].T
-        #print(Psorted.shape)
-        
-        # em ordem crescente
-        end_idx = int((p/100)*len(Psorted.T))
-        #print ("Os 95% mais próximos:", Psorted[:,:end_idx])
-        return (Psorted[:,:end_idx])
+    def report(self, infos, coefs_var, areas_medias):
+        print("nSamp \tMean \tStd \tTotal \tFeas \tMeanP1 \tMeanP2 \tMeanP3")
+        print('--------------------------------------------------------------')
+        for linha in infos:
+            print("%i \t%.3f \t%.3f \t%i \t%i \t%.3f \t%.3f \t%.3f" \
+              % (linha[0],linha[1],linha[2],linha[3],linha[4],linha[5],linha[6],linha[7]))
+        print('--------------------------------------------------------------')
+        print ("Áreas médias:", np.round(areas_medias,3))
+        print ("Desvios padrão:", np.round(coefs_var,3))
     
-    
+
+        
     def multi_runs(self, n, nY,nD,nE,nL):
         from scipy.spatial import ConvexHull
-        import numpy as np
         cv = lambda x: np.std(x) / np.mean(x) *100
+        
+	#repete a simulação n vezes variando nY
+        CVs_0= []; CVs_1= []; CVs_2= []; CVs_m= []
+        areas_medias_0 = []; areas_medias_1 = []; areas_medias_2 = []; 
+        infos_0 = []; infos_1 = []; infos_2 = []; 
+        areas_medias_m = []
+        
+        if isinstance(nY, list):
+            samples_sizes = nY.copy()
+            #self.Si = np.linalg.inv(np.cov(self.Y.T))
+            idx = 0
+        if isinstance(nD, list):
+            samples_sizes = nD.copy()
+            #self.Si = np.linalg.inv(np.cov(self.D.T))
+            idx = 1
+        if isinstance(nE, list):
+            samples_sizes = nE.copy()
+            #self.Si = np.linalg.inv(np.cov(self.E.T))
+            idx = 2
+        if isinstance(nL, list):
+            samples_sizes = nL.copy()
+            #self.Si = np.linalg.inv(np.cov(self.L.T))
+            idx = 3
 
-        areas = []
-        #areas_medias = []
-        for i in range(n):
-            #print ("Rodando simulação número:", i)
-            P = self.run(nY,nD,nE,nL)
-            P = self.confidence_region(P, p = 95)
+        print('Samples sizes:',samples_sizes)
+        for ss in samples_sizes:
+    	    areas_0 = []; areas_1 = []; areas_2 = []
+    	    
+    	    # define quem vai variar
+    	    if idx == 0: 
+    	        nY = ss
+    	    if idx == 1: 
+    	        nD = ss    
+    	    if idx == 2: 
+    	        nE = ss 
+    	    if idx == 3: 
+    	        nL = ss 
     
-            hull = ConvexHull(P.T)
-            areas.append(hull.volume)
-                
-        mean = np.round(np.mean(areas),3)
-        SD = np.round(np.std(areas),4)
-        P1 = np.round(np.mean(P[0]),3)
-        P2 = np.round(np.mean(P[1]),3)
-        P3 = np.round(1-P1-P2,3)
-        #print ('nY','nD','nE','nL','Mean','SD','Possible','Feasible','P1','P2','P3')
-        print (nY,nD,nE,nL, mean, SD, nY*nD*nE*nL, len(P.T), P1, P2, P3)
-        CV = cv(areas)     
-        return (CV)
+    	    print('Sample size:', ss)
+    	    for i in range(n):
+                Ys,Ds,Es,Ls = self.randon_choice(nY, nD, nE, nL) 
+                nSamples = nY*nD*nE*nL
+                #Ymean = Ys.mean(axis=0)
+        
+                # Solve = 0 
+                P_0 = self.run(Ys,Ds,Es,Ls, solve=0)
+                P_0 = self.confidence_region(P_0, p = 95)
+                points_0 = P_0.T   
+                hull_0 = ConvexHull(points_0)
+                areas_0.append(hull_0.volume)
+
+                # Solve = 1 
+                P_1 = self.run(Ys,Ds,Es,Ls, solve=1)
+                P_1 = self.confidence_region(P_1, p = 95)
+                points_1 = P_1.T   
+                hull_1 = ConvexHull(points_1)
+                areas_1.append(hull_1.volume)
+        
+                # Solve = 2 
+                P_2 = self.run(Ys,Ds,Es,Ls, solve=2)
+                P_2 = self.confidence_region(P_2, p = 95)
+                points_2 = P_2.T   
+                hull_2 = ConvexHull(points_2)
+                areas_2.append(hull_2.volume)
+
+    	    infos_0.append([ss, np.mean(areas_0),np.std(areas_0), nSamples, \
+    	                    len(points_0), np.mean(P_0[0]), np.mean(P_0[1]), \
+    	                    1-np.mean(P_0[0])- np.mean(P_0[1])])
+    	    infos_1.append([ss, np.mean(areas_1),np.std(areas_1), nSamples, \
+    	                    len(points_1), np.mean(P_1[0]), np.mean(P_1[1]), \
+    	                    1-np.mean(P_1[0])- np.mean(P_1[1])])
+    	    infos_2.append([ss, np.mean(areas_2),np.std(areas_2), nSamples, \
+    	                    len(points_2), np.mean(P_2[0]), np.mean(P_2[1]), \
+    	                    1-np.mean(P_2[0])- np.mean(P_2[1])])
+        
+    	    CVs_0.append(cv(areas_0))
+    	    areas_medias_0.append(np.mean(areas_0))
+
+    	    CVs_1.append(cv(areas_1))
+    	    areas_medias_1.append(np.mean(areas_1))
+    
+    	    CVs_2.append(cv(areas_2))
+    	    areas_medias_2.append(np.mean(areas_2))
+    
+        print("Done!")
+        
+        self.report(infos_0, CVs_0, areas_medias_0)
+        self.report(infos_1, CVs_1, areas_medias_1)
+        self.report(infos_2, CVs_2, areas_medias_2)
+        return (CVs_0,CVs_1, CVs_2)
+        
+       
             
             
             
